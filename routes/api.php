2ee42782fd4020db1189d49443ac855f9fd5bf7d
Joske <joskekostic@gmail.com>
2018-03-12 15:47:28 +0100

    -This commit will fix middleware /users route

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,6 @@ Route::post('/register', 'AuthController@register')->name('register');
 Route::post('user/register', 'APIRegisterController@register');
 Route::post('user/login', 'APILoginController@login');
 
-Route::middleware('jwt.auth')->get('users', function(Request $request) {
+Route::middleware('jwt.auth')->post('/users', function(Request $request) {
     return auth()->user();
 });
