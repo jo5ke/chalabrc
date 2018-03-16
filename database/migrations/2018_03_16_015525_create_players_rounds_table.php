be14627a82a062a41a625ca1a4ee3bd84d753d41
Joske <joskekostic@gmail.com>
2018-03-16 03:12:22 +0100

    -This commit will add stats fields in player_round pivot table.

--- database/migrations/2018_03_16_015525_create_players_rounds_table.php
+++ database/migrations/2018_03_16_015525_create_players_rounds_table.php
@@ -17,6 +17,30 @@ class CreatePlayersRoundsTable extends Migration
             $table->increments('id');
             $table->integer('round_id')->onDelete('cascade');
             $table->integer('player_id')->onDelete('cascade');
+            $table->integer('start')->unsigned()->nullable();
+            $table->integer('sub')->unsigned()->nullable();
+            $table->integer('assist')->unsigned()->nullable();
+            $table->integer('miss')->unsigned()->nullable();
+            $table->integer('k_score')->unsigned()->nullable();
+            $table->integer('d_score')->unsigned()->nullable();
+            $table->integer('m_score')->unsigned()->nullable();
+            $table->integer('a_score')->unsigned()->nullable();
+            $table->integer('kd_clean')->unsigned()->nullable();
+            $table->integer('m_clean')->unsigned()->nullable();
+            $table->integer('k_save')->unsigned()->nullable();
+            $table->integer('kd_clean')->unsigned()->nullable();
+            $table->integer('kd_3strike')->unsigned()->nullable();
+            $table->integer('yellow')->unsigned()->nullable();
+            $table->integer('red')->unsigned()->nullable();
+            $table->integer('own_goal')->unsigned()->nullable();
+            $table->integer('captain')->unsigned()->nullable();
+
+
+
+            
+            
+
+                        
             $table->timestamps();
         });
     }
